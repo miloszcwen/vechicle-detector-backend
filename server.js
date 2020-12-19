f838dbbefb75eb28618335cfbc9ae63c16e9116f
miloszcwen <cwen.milosz@gmail.com>
2020-12-19 16:45:50 +0100

    initial commit server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('app is working');
+})
+
+app.listen(3000, ()=>{
+    console.log('app is listening on port 3000');
+})
