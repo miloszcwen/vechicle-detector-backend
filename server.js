883b7f3dc25b7915db6c569cc484b8a41adb076c
miloszcwen <cwen.milosz@gmail.com>
2021-01-06 17:51:52 +0100

    connect signin to db

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const {database} = require('./database.js');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
@@ -14,8 +13,6 @@ const db = knex({
       database : 'vechicle-detector'
     }
   });
-// show all
- // db.select('*').from('users').then(data => {console.log(data);});
 
 const app = express();
 
@@ -24,25 +21,28 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('success');
 })
 
 app.post('/signin', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    let isValid = false;
-    // bcrypt.compareSync("B4c0/\/", hash); // true
-
-    let user = {};
-    // foreach not nessesary, DB will take care of it
-    database.users.forEach((element)=>{
-        if (email === element.email &&
-            password === element.password) {
-                isValid=true;
-                user = element;
-            }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', email)
+    .then(data=>{
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users')
+            .where('email', '=', email)
+            .then(user=>{
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'));
+        } else{
+            res.status(400).json('wrong credentials')
+        }
     })
-    isValid ? res.json(user) : res.status(400).json('email or password not valid');
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
