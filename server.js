1f3ffd5486c967f73ba02a74524702872705f44d
miloszcwen <cwen.milosz@gmail.com>
2021-01-07 20:40:17 +0100

    move register to ./controllers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/register')
 const { response } = require('express');
 
 const db = knex({
@@ -45,33 +46,8 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx=>{
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'));
-})
+app.post('/register', (req, res)=>
+{register.handleRegister(req,res,db,bcrypt)})
 
 
 app.get('/profile/:id', (req, res) => {
@@ -92,10 +68,10 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req, res)=>{
     const {id} = req.body;
     db('users').where('id', '=', id)
-    .increment('carcount', 1)
-    .returning('carcount')
-    .then(carcount=>{
-        return res.json(carcount[0]);
+    .increment('carCounter', 1)
+    .returning('carCounter')
+    .then(carCounter=>{
+        return res.json(carCounter[0]);
     })
     .catch(err => res.status(400).json(err))
     })
