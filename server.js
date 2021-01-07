2df8c24ae4fd0b4723edffcc4ce89a0adce6f782
miloszcwen <cwen.milosz@gmail.com>
2021-01-07 20:56:51 +0100

    move signin to ./controllers

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
-const register = require('./controllers/register')
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
 const { response } = require('express');
 
 const db = knex({
@@ -25,29 +26,11 @@ app.get('/', (req, res) => {
     res.send('success');
 })
 
-app.post('/signin', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data=>{
-        const isValid = bcrypt.compareSync(password, data[0].hash);
-        if (isValid) {
-            return db.select('*').from('users')
-            .where('email', '=', email)
-            .then(user=>{
-                res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'));
-        } else{
-            res.status(400).json('wrong credentials')
-        }
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', (req, res)=>{
+    signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req, res)=>
-{register.handleRegister(req,res,db,bcrypt)})
+app.post('/register', (req, res)=>{
+    register.handleRegister(req,res,db,bcrypt)})
 
 
 app.get('/profile/:id', (req, res) => {
