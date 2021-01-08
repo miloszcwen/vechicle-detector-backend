6614ab7d747de132927034c576dc33ca567b69ec
miloszcwen <cwen.milosz@gmail.com>
2021-01-08 14:52:32 +0100

    add env port

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.post('/imageurl', (req, res)=>{
     image.handleApiCall(req, res)})
 
 
-app.listen(3000, ()=>{
-    console.log('app is listening on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`app is listening on port 3000${process.env.PORT}`);
 })
