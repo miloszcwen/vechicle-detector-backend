877422912ec41868004be54b1be12f8e7550029f
miloszcwen <cwen.milosz@gmail.com>
2021-01-08 12:21:01 +0100

    move crarifai API calls to backend, create .env to store api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const Clarifai = require('clarifai');
+const dotenv = require('dotenv').config();
+
+
+const app = new Clarifai.App({
+    apiKey: process.env.ClarifAI_API_KEY
+   });
+
+   const handleApiCall = (req, res) => {
+       app.models.predict(
+           'f950c58836faebadfdd44d709fb7cef6',req.body.input)
+           .then(data => {
+             res.json(data);
+           })
+           .catch(err => res.status(400).json('unable to work with API'))
+   }
+
 const handleImage = (req, res, db)=>{
     const {id} = req.body;
     db('users').where('id', '=', id)
--- server.js
+++ server.js
@@ -40,6 +40,9 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req, res)=>{
     image.handleImage(req, res, db)})
 
+app.post('/imageurl', (req, res)=>{
+    image.handleApiCall(req, res)})
+
 
 app.listen(3000, ()=>{
     console.log('app is listening on port 3000');
