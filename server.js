dfa050f9137ebc83f70d948db6d718e68d9bce77
miloszcwen <cwen.milosz@gmail.com>
2021-01-11 16:15:18 +0100

    add heroku db

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('success!')})
