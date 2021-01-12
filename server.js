8664adba4df3bc2f4b36198782fde4801d8f5e00
miloszcwen <cwen.milosz@gmail.com>
2021-01-12 14:25:22 +0100

    add reject unauthorized: false

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ssl: true
-      // ssl: {
-      //   rejectUnauthorized: false
-      // }
+    //   ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
